75b90564f00b0184188f4eef24b815184f2b4cf2
snorkelingcode <daneknudsen3d@gmail.com>
2025-02-06 17:17:23 -0600

    test

--- server.js
+++ server.js
@@ -18,13 +18,13 @@ const corsOptions = {
         "https://fe-slacker-drw0hwhl1-snorkelingcodes-projects.vercel.app",
         "http://localhost:3000"
     ],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: [
-        'Origin', 
-        'X-Requested-With', 
-        'Content-Type', 
-        'Accept', 
-        'Authorization'
+        "Origin",
+        "X-Requested-With",
+        "Content-Type",
+        "Accept",
+        "Authorization"
     ],
     credentials: true,
     optionsSuccessStatus: 200
@@ -35,6 +35,11 @@ app.options('*', cors(corsOptions));
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
+
+// Test route
+app.get('/test', (req, res) => {
+    res.json({ message: 'Test endpoint working' });
+});
 
 // Request logging
 app.use((req, res, next) => {
