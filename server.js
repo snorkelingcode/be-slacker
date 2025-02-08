c0da306a9293171ddda69f9715081130160426bd
snorkelingcode <daneknudsen3d@gmail.com>
2025-02-07 23:24:22 -0600

    allow CORS from anyone (test)

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ connectDB();
 
 // CORS Setup
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://fe-slacker.vercel.app');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, Accept');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
