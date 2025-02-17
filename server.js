cf600e944057cc48a9e04e55aeb81bfdb3399011
snorkelingcode <daneknudsen3d@gmail.com>
2025-02-16 22:09:12 -0600

    fixed a spelling error

--- server.js
+++ server.js
@@ -170,7 +170,7 @@ app.post('/api/upload/:type', upload.single('file'), async (req, res) => {
 // Import and use routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/posts', require('./routes/posts'));
-app.use('/api/notifications', require('/routes/notifications'));
+app.use('/api/notifications', require('./routes/notifications'));
 
 // Global error handler
 app.use((err, req, res, next) => {
