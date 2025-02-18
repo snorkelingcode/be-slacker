edb87132edf59023aa57af3a63a08a9be5da7b2e
snorkelingcode <daneknudsen3d@gmail.com>
2025-02-17 19:32:28 -0600

    added wav format

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -14,7 +14,7 @@ const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
         folder: 'slacker',
-        allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'mp4', 'mov', 'mp3', 'mkv'],
+        allowed_formats: ['jpg', 'jpeg', 'png', 'gif', 'mp4', 'mov', 'mp3', 'mkv', 'wav'],
         resource_type: 'auto',
         transformation: [{ quality: 'auto' }]
     }
